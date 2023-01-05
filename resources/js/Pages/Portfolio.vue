2a85a56996ce4410c0e696d57a57f92177f920ce
Marus Workshop <107907291+MarusWorkshop@users.noreply.github.com>
2023-01-06 05:11:08 +0800

    Update

--- resources/js/Pages/Portfolio.vue
+++ resources/js/Pages/Portfolio.vue
@@ -4,7 +4,7 @@
             <div class="flex items-center relative">
                 <img class="h-[100%] w-[100%] object-fit rounded-xl" src="https://source.unsplash.com/random/12" />
                 <div class="w-40 h-40 bg-[#D9D9D9] rounded-xl absolute bottom-5 left-5">
-                    <img class="h-full w-full object-cover rounded-xl shadow-xl" src="https://source.unsplash.com/random/42" />
+                    <img class="h-full w-full object-cover rounded-xl" src="https://source.unsplash.com/random/42" />
                 </div>
             </div>
         </div>
@@ -19,7 +19,7 @@
 
         <div class="columns-2 p-4 md:columns-3xs grow overflow-y-scroll pb-24">
             <div class="relative mb-6 space-y-4 rounded-xl">
-                <img class="rounded-xl shadow-md" src="https://source.unsplash.com/random/1" />
+                <img class="rounded-xl" src="https://source.unsplash.com/random/1" />
                 <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
                     <div class="flex h-full w-6 md:w-10 shrink-0 shrink-0 items-center justify-center overflow-hidden rounded-full">
                         <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
@@ -32,7 +32,7 @@
             </div>
 
             <div class="relative mb-6 space-y-4 rounded-xl">
-                <img class="rounded-xl shadow-md" src="https://source.unsplash.com/random/2" />
+                <img class="rounded-xl" src="https://source.unsplash.com/random/2" />
                 <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
                     <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
                         <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
@@ -45,7 +45,7 @@
             </div>
 
             <div class="relative mb-6 space-y-4 rounded-xl">
-                <img class="rounded-xl shadow-md" src="https://source.unsplash.com/random/3" />
+                <img class="rounded-xl" src="https://source.unsplash.com/random/3" />
                 <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
                     <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
                         <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
@@ -58,7 +58,7 @@
             </div>
 
             <div class="relative mb-6 space-y-4 rounded-xl">
-                <img class="rounded-xl shadow-md" src="https://source.unsplash.com/random/4" />
+                <img class="rounded-xl" src="https://source.unsplash.com/random/4" />
                 <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
                     <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
                         <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
@@ -71,7 +71,7 @@
             </div>
 
             <div class="relative mb-6 space-y-4 rounded-xl">
-                <img class="rounded-xl shadow-md" src="https://source.unsplash.com/random/5" />
+                <img class="rounded-xl" src="https://source.unsplash.com/random/5" />
                 <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
                     <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
                         <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
@@ -84,7 +84,7 @@
             </div>
 
             <div class="relative mb-6 space-y-4 rounded-xl">
-                <img class="rounded-xl shadow-md" src="https://source.unsplash.com/random/6" />
+                <img class="rounded-xl" src="https://source.unsplash.com/random/6" />
                 <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
                     <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
                         <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
@@ -97,7 +97,7 @@
             </div>
 
             <div class="relative mb-6 space-y-4 rounded-xl">
-                <img class="rounded-xl shadow-md" src="https://source.unsplash.com/random/7" />
+                <img class="rounded-xl" src="https://source.unsplash.com/random/7" />
                 <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
                     <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
                         <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
@@ -110,7 +110,7 @@
             </div>
 
             <div class="relative mb-6 space-y-4 rounded-xl">
-                <img class="rounded-xl shadow-md" src="https://source.unsplash.com/random/8" />
+                <img class="rounded-xl" src="https://source.unsplash.com/random/8" />
                 <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
                     <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
                         <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
@@ -123,7 +123,7 @@
             </div>
 
             <div class="relative mb-6 space-y-4 rounded-xl">
-                <img class="rounded-xl shadow-md" src="https://source.unsplash.com/random/9" />
+                <img class="rounded-xl" src="https://source.unsplash.com/random/9" />
                 <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
                     <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
                         <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
@@ -136,7 +136,7 @@
             </div>
 
             <div class="relative mb-6 space-y-4 rounded-xl">
-                <img class="rounded-xl shadow-md" src="https://source.unsplash.com/random/10" />
+                <img class="rounded-xl" src="https://source.unsplash.com/random/10" />
                 <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
                     <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
                         <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 //Route::get('/', [MainController::class, 'hub']);
 //Route::get('/profile', [MainController::class, 'profile']);
+
 Route::inertia('/','Hub');
-Route::inertia('/explore','Explore');
-Route::inertia('/events','Events');
-Route::inertia('/jobs','Jobs');
 Route::inertia('/portfolio','Portfolio');
 
