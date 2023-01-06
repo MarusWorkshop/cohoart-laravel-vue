70a06c21fb2ad0a97ccd3d910c10e7187e8400ae
Marus Workshop <107907291+MarusWorkshop@users.noreply.github.com>
2023-01-06 23:37:42 +0800

    Update

--- resources/js/Pages/Hub.vue
+++ resources/js/Pages/Hub.vue
@@ -29,6 +29,136 @@
                         </div>
                     </div>
 
+                    <div class="relative mb-6 space-y-4 rounded-xl">
+                        <img class="rounded-xl" src="https://source.unsplash.com/random/1" />
+                        <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
+                            <div class="flex h-full w-6 md:w-10 shrink-0 shrink-0 items-center justify-center overflow-hidden rounded-full">
+                                <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
+                            </div>
+                            <div class="text-gray-500">
+                                <h1 class="text-[8px] font-black">Iqmal Nazlan</h1>
+                                <h2 class="text-sm hidden md:block">Full-Stack developer</h2>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="relative mb-6 space-y-4 rounded-xl">
+                        <img class="rounded-xl" src="https://source.unsplash.com/random/2" />
+                        <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
+                            <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
+                                <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
+                            </div>
+                            <div class="text-gray-500">
+                                <h1 class="text-[8px] font-black">Iqmal Nazlan</h1>
+                                <h2 class="text-sm hidden md:block">Full-Stack developer</h2>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="relative mb-6 space-y-4 rounded-xl">
+                        <img class="rounded-xl" src="https://source.unsplash.com/random/3" />
+                        <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
+                            <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
+                                <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
+                            </div>
+                            <div class="text-gray-500">
+                                <h1 class="text-[8px] font-black">Iqmal Nazlan</h1>
+                                <h2 class="text-sm hidden md:block">Full-Stack developer</h2>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="relative mb-6 space-y-4 rounded-xl">
+                        <img class="rounded-xl" src="https://source.unsplash.com/random/4" />
+                        <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
+                            <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
+                                <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
+                            </div>
+                            <div class="text-gray-500">
+                                <h1 class="text-[8px] font-black">Iqmal Nazlan</h1>
+                                <h2 class="text-sm hidden md:block">Full-Stack developer</h2>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="relative mb-6 space-y-4 rounded-xl">
+                        <img class="rounded-xl" src="https://source.unsplash.com/random/5" />
+                        <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
+                            <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
+                                <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
+                            </div>
+                            <div class="text-gray-500">
+                                <h1 class="text-[8px] font-black">Iqmal Nazlan</h1>
+                                <h2 class="text-sm hidden md:block">Full-Stack developer</h2>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="relative mb-6 space-y-4 rounded-xl">
+                        <img class="rounded-xl" src="https://source.unsplash.com/random/6" />
+                        <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
+                            <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
+                                <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
+                            </div>
+                            <div class="text-gray-500">
+                                <h1 class="text-[8px] font-black">Iqmal Nazlan</h1>
+                                <h2 class="text-sm hidden md:block">Full-Stack developer</h2>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="relative mb-6 space-y-4 rounded-xl">
+                        <img class="rounded-xl" src="https://source.unsplash.com/random/7" />
+                        <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
+                            <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
+                                <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
+                            </div>
+                            <div class="text-gray-500">
+                                <h1 class="text-[8px] font-black">Iqmal Nazlan</h1>
+                                <h2 class="text-sm hidden md:block">Full-Stack developer</h2>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="relative mb-6 space-y-4 rounded-xl">
+                        <img class="rounded-xl" src="https://source.unsplash.com/random/8" />
+                        <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
+                            <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
+                                <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
+                            </div>
+                            <div class="text-gray-500">
+                                <h1 class="text-[8px] font-black">Iqmal Nazlan</h1>
+                                <h2 class="text-sm hidden md:block">Full-Stack developer</h2>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="relative mb-6 space-y-4 rounded-xl">
+                        <img class="rounded-xl" src="https://source.unsplash.com/random/9" />
+                        <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
+                            <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
+                                <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
+                            </div>
+                            <div class="text-gray-500">
+                                <h1 class="text-[8px] font-black">Iqmal Nazlan</h1>
+                                <h2 class="text-sm hidden md:block">Full-Stack developer</h2>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="relative mb-6 space-y-4 rounded-xl">
+                        <img class="rounded-xl" src="https://source.unsplash.com/random/10" />
+                        <div class="flex h-6 md:h-10 items-center justify-start space-x-2">
+                            <div class="flex h-full w-6 md:w-10 shrink-0 items-center justify-center overflow-hidden rounded-full">
+                                <img class="h-full w-full object-cover" src="https://source.unsplash.com/random/2" />
+                            </div>
+                            <div class="text-gray-500">
+                                <h1 class="text-[8px] font-black">Iqmal Nazlan</h1>
+                                <h2 class="text-sm hidden md:block">Full-Stack developer</h2>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
